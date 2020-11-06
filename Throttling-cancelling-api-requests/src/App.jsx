44544c82679f5fccb77ec6b2757e81a045d1e1ac
dradhakrishnan <v-dradhakrishnan@egencia.com>
2020-11-06 12:39:40 +0530

    Cancelling and throttling axios API call

---
+++ Throttling-cancelling-api-requests/src/App.jsx
@@ -0,0 +1,72 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const App = () => {
+
+
+    const [term, setTerm] = useState("");  // state, setState()
+
+//useEffect()
+    useEffect(
+        () => {
+                //Action to perform
+            console.log("Inside use effect")
+            //const cancelTokenSource = axios.CancelToken.source();
+            const search = async function() {
+                const response = await axios.get("https://en.wikipedia.org/w/api.php" , {
+                    //cancelToken: cancelTokenSource.token,
+                    params : {
+                        action: "query",
+                        list: "search",
+                        origin: '*',
+                        format: 'json',
+                        srsearch: term
+
+                    }
+                });
+                console.log(response.data.query.search)
+            }
+
+            let apiIntervel = setTimeout(()=>{
+                if(term) {
+                    search();
+                }
+            }, 1000)
+
+
+
+            return () => {
+                console.log("Inside clean up function");
+                clearTimeout(apiIntervel)
+                //cancelTokenSource.cancel("Aborting the api");
+            }
+
+
+        },[term]
+         // optional Array ==> nothing  [] [state]
+    )
+
+
+    return(
+        <div className="ui two column centered grid">
+            <div className="column">
+                <div className="ui form">
+                    <div className="field">
+                        <label>Enter Search Term</label>
+                        <input
+                            value={term}
+                            onChange={(e) => setTerm(e.target.value)}
+                            className="input"
+                        />
+                    </div>
+
+                </div>
+                <div className="ui celled-list">
+                    {term}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default App;
